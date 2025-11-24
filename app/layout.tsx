5f18c4a8f9021c00cb30f513601c444faf577a6a
ArianaPolar-dev <polar3dev@gmail.com>
2025-11-24 12:59:05 -0400

    tipado en RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-// app/layout.tsx
 import './globals.css';
+import type { ReactNode } from 'react';
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es">
       <body>{children}</body>
